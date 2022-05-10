2727ad47ba3d3956ce480c605e7c206ec0fd5089
Renata <rvaderna@openlawlib.org>
2022-05-10 07:08:34 +0200

    feat: add initial test projects

---
+++ upgrade/tests/data/dependency2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from pathlib import Path
+this_dir = Path(__file__).absolute().parent
+
+
+if __name__ == "__main__":
+    setup(
+        data_files=[
+            ('lib/site-packages/oll',
+            ['./README.md', './constraints.txt'])
+        ]
+    )
