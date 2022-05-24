59c8158f0ff08053554c58f70726382f509c8762
n-dusan <nikolic.dusan.dey@gmail.com>
2022-05-24 20:32:01 +0200

    feat: create and cleanup venv
    *For each session, create a fresh venv and install required packages. VEnv is cleaned up after session.

--- upgrade/tests/conftest.py
+++ upgrade/tests/conftest.py
@@ -1,15 +1,28 @@
 import os
+import shutil
+import sys
 import pytest
 from pathlib import Path
-from upgrade.scripts.upgrade_python_package import pip
+from upgrade.scripts.upgrade_python_package import pip, run
 
 CLOUDSMITH_KEY = os.environ.get("CLOUDSMITH_KEY")
+
 REPOSITORY_WHEELS_PATH = Path(__file__).parent / "repository"
+VENV_PATH = Path(__file__).parent / "venv"
 
 
 @pytest.fixture
 def wheels_dir():
     return REPOSITORY_WHEELS_PATH
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_venv():
+    if not VENV_PATH.exists():
+        os.makedirs(VENV_PATH, exist_ok=True)
+    run(sys.executable, "-m", "venv", str(VENV_PATH))
+    yield
+    shutil.rmtree(VENV_PATH)
 
 
 @pytest.fixture
