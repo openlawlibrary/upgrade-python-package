eff537deffb3fa55735ef76f5416e3dab515ba0e
n-dusan <nikolic.dusan.dey@gmail.com>
2022-05-26 03:11:11 +0200

    feat: `install_local_package` util, venv fixtures

--- upgrade/tests/conftest.py
+++ upgrade/tests/conftest.py
@@ -9,6 +9,12 @@ CLOUDSMITH_KEY = os.environ.get("CLOUDSMITH_KEY")
 
 REPOSITORY_WHEELS_PATH = Path(__file__).parent / "repository"
 VENV_PATH = Path(__file__).parent / "venv"
+original_executable = sys.executable
+
+
+def pytest_configure(config):
+    sys.executable = str(VENV_PATH / "Scripts" / "python.exe")
+    sys.modules["oll"] = None
 
 
 @pytest.fixture
@@ -20,15 +26,22 @@ def wheels_dir():
 def setup_venv():
     if not VENV_PATH.exists():
         os.makedirs(VENV_PATH, exist_ok=True)
-    run(sys.executable, "-m", "venv", str(VENV_PATH))
+    run(original_executable, "-m", "venv", str(VENV_PATH))
     yield
     shutil.rmtree(VENV_PATH)
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def use_pip():
     pip("install", "--upgrade", "pip")
     yield pip
     pip("uninstall", "-y", "oll-test-top-level")
     pip("uninstall", "-y", "oll-dependency1")
     pip("uninstall", "-y", "oll-dependency2")
+
+
+def install_local_package(dependency):
+    full_dep_path = str(REPOSITORY_WHEELS_PATH / dependency)
+    links_path = str(REPOSITORY_WHEELS_PATH)
+
+    pip("install", full_dep_path, "--find-links", links_path)
