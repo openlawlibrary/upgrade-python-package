518a76dbeb068d27756924187db39e271c605726
Renata <rvaderna@openlawlib.org>
2022-06-24 17:44:20 +0200

    Python 3.10 support and remove 3.6
    Pillow dependency had to be updated, since supports Python versions up to 3.8.
    Generate new test wheels

--- upgrade/tests/test_integ_upgrade_from_local_wheel.py
+++ upgrade/tests/test_integ_upgrade_from_local_wheel.py
@@ -79,7 +79,7 @@ def test_upgrade_local_wheels_top_level_package_from_2_0_1_to_2_0_2_expect_error
     )
     out, _ = capfd.readouterr()
 
-    assert f"Failed to install wheel {package}" in out
+    assert f"Failed to install wheel" in out
     assert "Successfully uninstalled oll-test-top-level-2.0.2" in out
     assert "Successfully installed oll-test-top-level-2.0.1" in out
 
