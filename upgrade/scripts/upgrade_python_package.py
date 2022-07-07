3163192f42991632e625f515e5f4094703546827
n-dusan <nikolic.dusan.dey@gmail.com>
2022-07-07 19:24:25 +0200

    fix: lazy load requests module
    *If upgrade python package script is installed without a requests module, lazy load the import into the function. This can happen if script is being used by a very old `setuptools`<50 version

--- upgrade/scripts/upgrade_python_package.py
+++ upgrade/scripts/upgrade_python_package.py
@@ -8,7 +8,6 @@ import glob
 import argparse
 from importlib import util
 from pathlib import Path
-import requests
 import site
 
 
@@ -207,6 +206,11 @@ def install_wheel(
 
 
 def is_cloudsmith_url_valid(cloudsmith_url):
+    try:
+        import requests
+    except ImportError:
+        logging.error("Module 'requests' not found. Could not validate cloudsmith url.")
+        return None
     response = requests.get(cloudsmith_url)
     if response.status_code != 200:
         raise Exception(
