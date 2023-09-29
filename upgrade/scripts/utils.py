6279cd860513a8dfcfc1c9c434075c78fb80fffb
n-dusan <nikolic.dusan.dey@gmail.com>
2023-09-29 19:24:44 +0200

    feat: add util functions for working with venv

--- upgrade/scripts/utils.py
+++ upgrade/scripts/utils.py
@@ -1,5 +1,24 @@
+import logging
+from pathlib import Path
 from sys import platform
 
+logger = logging.getLogger(__name__)
 
-def is_windows():
+
+def create_directory(path: Path) -> None:
+    try:
+        path.mkdir(parents=True)
+    except Exception as e:
+        logger.error("Failed to create virtualenv directory: %s", e)
+        raise e
+
+
+def platform_specific_python_path(venv_path: str) -> str:
+    if is_windows():
+        return str(Path(venv_path, "Scripts", "python.exe"))
+    else:
+        return str(Path(venv_path, "bin", "python"))
+
+
+def is_windows() -> bool:
     return platform == "win32" or platform == "cygwin"
