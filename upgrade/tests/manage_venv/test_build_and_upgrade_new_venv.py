509bc11e9698534455e99db87956ee6876044bcb
n-dusan <nikolic.dusan.dey@gmail.com>
2023-10-17 18:17:34 +0200

    refact(test): rename `managevenv` new vens, add remaining test cases

--- upgrade/tests/manage_venv/test_build_and_upgrade_new_venv.py
+++ upgrade/tests/manage_venv/test_build_and_upgrade_new_venv.py
@@ -1,5 +1,5 @@
 import pytest
-import mock
+from mock import patch
 
 from upgrade.scripts.utils import is_windows
 from upgrade.scripts.manage_venv import (
@@ -19,26 +19,29 @@ EXPECTED_VENV_FILES = (
 @pytest.mark.parametrize(
     "dependency_to_install, expected_installed_version",
     [
+        ("oll-test-top-level==2.0.0", "2.0.0"),
         ("oll-test-top-level==2.0.1", "2.0.1"),
-        # ("oll-test-top-level~=2.0.1", "2.0.1"), # TODO: make top-level 2.0.2 into 2.1.0 
-        # ("oll-test-top-level==3.0.1", "3.0.1"), 
+        ("oll-test-top-level~=2.0.0", "2.0.1"),
+        ("oll-test-top-level~=2.0.1", "2.0.1"),
     ],
 )
-def test_create_venv_when_not_exists_expect_created(
-    dependency_to_install, expected_installed_version, envs_home, wheels_dir
+def test_build_and_upgrade_venv_where_venv_did_not_exist_and_auto_upgrade_is_disabled_expect_venv_created_and_upgraded(
+    dependency_to_install,
+    expected_installed_version,
+    envs_home,
+    wheels_dir,
+    mock_cloudsmith_url_valid,
 ):
     cut = build_and_upgrade_venv
 
-    with mock.patch(
+    with patch(
         "upgrade.scripts.manage_venv.determine_compatible_upgrade_version",
         lambda *_,: expected_installed_version,
-    ), mock.patch(
-        "upgrade.scripts.validations.is_cloudsmith_url_valid", lambda *_,: True
     ):
         cut(
             dependency_to_install,
             envs_home,
-            auto_upgrade=True,
+            auto_upgrade=False,
             cloudsmith_url="",
             wheels_path=str(wheels_dir),
             update_from_local_wheels=True,
@@ -51,7 +54,6 @@ def test_create_venv_when_not_exists_expect_created(
     for file in EXPECTED_VENV_FILES:
         assert Path(venv_path, file).exists()
 
-    breakpoint()
     venv_executable = _get_venv_executable(venv_path)
 
     dependencies_from_venv = venv_pip(
@@ -61,6 +63,13 @@ def test_create_venv_when_not_exists_expect_created(
         "--exclude-editable",
     ).splitlines()
 
-    assert f"oll-test-top-level=={expected_installed_version}" in dependencies_from_venv
-    assert f"oll-dependency1=={expected_installed_version}" in dependencies_from_venv
-    assert f"oll-dependency2=={expected_installed_version}" in dependencies_from_venv
+    expected_packages = {
+        f"oll-test-top-level=={expected_installed_version}",
+        f"oll-dependency1=={expected_installed_version}",
+        f"oll-dependency2=={expected_installed_version}",
+    }
+    actual_packages = set(dependencies_from_venv)
+
+    expected = True
+    actual = expected_packages.issubset(actual_packages)
+    assert actual == expected
