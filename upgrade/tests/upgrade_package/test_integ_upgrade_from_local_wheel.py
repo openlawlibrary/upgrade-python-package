5c62cc99b87987d33c25efda8782970a529a2eda
n-dusan <nikolic.dusan.dey@gmail.com>
2023-10-17 12:34:18 +0200

    fix(test): rename `oll-test-top-level` from `2.0.2` to `2.1.0`
    Necessary because we consider the previous `2.0.2` version incompatible
    during upgrade tests. To make it more clear, rename the version to
    `2.1.0` and update existing testing framework.

--- upgrade/tests/upgrade_package/test_integ_upgrade_from_local_wheel.py
+++ upgrade/tests/upgrade_package/test_integ_upgrade_from_local_wheel.py
@@ -5,11 +5,15 @@ def test_upgrade_local_wheels_top_level_package_from_2_0_0_to_2_0_1_expect_succe
     wheels_dir, use_pip, capfd, mocked_constraints_path, mock_find_spec
 ):
     package = "oll-test-top-level==2.0.0"
-    upgrade_from_local_wheel(
+    success, _ = upgrade_from_local_wheel(
         package,
         skip_post_install=False,
         wheels_path=str(wheels_dir),
+        version="==2.0.0",
     )
+
+    assert success
+
     use_pip("check")
     out, _ = capfd.readouterr()
 
@@ -29,11 +33,15 @@ def test_upgrade_local_wheels_top_level_package_from_2_0_0_to_2_0_1_expect_succe
 
     # now upgrade to newer version
     package = "oll-test-top-level==2.0.1"
-    upgrade_from_local_wheel(
+    success, _ = upgrade_from_local_wheel(
         package,
         skip_post_install=False,
         wheels_path=str(wheels_dir),
+        version="==2.0.1",
     )
+
+    assert success
+
     use_pip("check")
     out, _ = capfd.readouterr()
 
@@ -56,11 +64,15 @@ def test_upgrade_local_wheels_top_level_package_from_2_0_1_to_2_0_2_expect_error
     wheels_dir, use_pip, capfd, mocked_constraints_path
 ):
     package = "oll-test-top-level==2.0.1"
-    upgrade_from_local_wheel(
+    success, _ = upgrade_from_local_wheel(
         package,
         skip_post_install=False,
         wheels_path=str(wheels_dir),
+        version="==2.0.1",
     )
+
+    assert success
+
     use_pip("check")
     dependencies_from_venv = use_pip(
         "list",
@@ -71,16 +83,20 @@ def test_upgrade_local_wheels_top_level_package_from_2_0_1_to_2_0_2_expect_error
     assert "oll-dependency1==2.0.1" in dependencies_from_venv
     assert "oll-dependency2==2.0.1" in dependencies_from_venv
 
-    package = "oll-test-top-level==2.0.2"
-    upgrade_from_local_wheel(
+    package = "oll-test-top-level==2.1.0"
+    success, _ = upgrade_from_local_wheel(
         package,
         skip_post_install=False,
         wheels_path=str(wheels_dir),
+        version="==2.1.0",
     )
+
+    assert not success
+
     out, _ = capfd.readouterr()
 
     assert f"Failed to install wheel" in out
-    assert "Successfully uninstalled oll-test-top-level-2.0.2" in out
+    assert "Successfully uninstalled oll-test-top-level-2.1.0" in out
     assert "Successfully installed oll-test-top-level-2.0.1" in out
 
     dependencies_from_venv = use_pip(
@@ -89,6 +105,6 @@ def test_upgrade_local_wheels_top_level_package_from_2_0_1_to_2_0_2_expect_error
         "--exclude-editable",
     ).splitlines()
 
-    assert "oll-test-top-level==2.0.2" not in dependencies_from_venv
+    assert "oll-test-top-level==2.1.0" not in dependencies_from_venv
     assert "oll-test-top-level==2.0.1" in dependencies_from_venv
     use_pip("check")
